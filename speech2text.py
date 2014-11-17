442b17d784874c531f20791adfb16e031400081d
Kenny Lu <luzhuomi@gmail.com>
2014-11-17 07:59:17 +0000

    added facerec.py

--- speech2text.py
+++ speech2text.py
@@ -2,7 +2,7 @@ import urllib2
 import sys
 
 url = "https://www.google.com/speech-api/v1/recognize?xjerr=1&client=chromium&lang=en-US"
-audio = open(sys.argv[0],'rb').read()
+audio = open(sys.argv[1],'rb').read()
 headers={'Content-Type': 'audio/x-flac; rate=16000', 'User-Agent':'Mozilla/5.0'}
 request = urllib2.Request(url, data=audio, headers=headers)
 response = urllib2.urlopen(request)
