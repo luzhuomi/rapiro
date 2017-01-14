7d751890eade1b4cf801184863a693182f70085c
Kenny Lu <luzhuomi@gmail.com>
2017-01-14 08:49:41 +0800

    changed from festival to flite

--- text2speech.py
+++ text2speech.py
@@ -5,7 +5,7 @@ DEVNULL = open(os.devnull, 'wb')
 
 def speak(t):
 	# p = subprocess.Popen(["sudo","festival", "-b", "(SayText \"" + t + "\")"], stdin=subprocess.PIPE, stdout=DEVNULL, stderr=DEVNULL)
-	p = subprocess.Popen(["sudo","flite", "-voice rms -t", "\"" + t + "\""], stdin=subprocess.PIPE, stdout=DEVNULL, stderr=DEVNULL)
+	p = subprocess.Popen(["sudo","flite", "-voice", "rms",  "-t", "\"" + t + "\""], stdin=subprocess.PIPE, stdout=DEVNULL, stderr=DEVNULL)
 	return p.wait()
 
 #speak("hi")
