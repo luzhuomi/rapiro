68bc2777dfbed21b68b3637f2e3aeec24cef4887
Kenny Lu <luzhuomi@gmail.com>
2017-01-21 22:30:13 +0800

    updated installation instructions

--- chromium.sh
+++ chromium.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-/usr/bin/chromium-browser  https://127.0.0.1:8080/prompt 
+/usr/bin/chromium-browser  https://127.0.0.1:8080/prompt --ignore-certificate-errors
 #chromium-browser www.youtube.com
